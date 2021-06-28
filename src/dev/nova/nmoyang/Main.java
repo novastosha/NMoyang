9ad5c1d040d7a383afb72e207cadef75feee5680
novastosha <galliigotu@gmail.com>
2021-06-28 19:01:14 +0100

    Creating a global API instance on the startup.

--- src/dev/nova/nmoyang/Main.java
+++ src/dev/nova/nmoyang/Main.java
@@ -1,8 +1,14 @@
 package dev.nova.nmoyang;
 
+import dev.nova.nmoyang.api.API;
+import dev.nova.nmoyang.gui.GUI;
+
 public class Main {
 
+    private static API API;
+
     public static void main(String[] args) {
+        API = new API();
         if(args.length == 0){
             guiMode();
         }else if (args.length == 1 && args[0].equalsIgnoreCase("consoleMode")){
@@ -13,9 +19,14 @@ public class Main {
     }
 
     private static void consoleMode() {
+
     }
 
     private static void guiMode() {
+        GUI gui = new GUI();
     }
 
+    public static API getAPI() {
+        return API;
+    }
 }
