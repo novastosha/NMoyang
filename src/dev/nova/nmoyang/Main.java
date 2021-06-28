db28c50dc3fcdbcd2e198bf3c7a4d73405290966
novastosha <galliigotu@gmail.com>
2021-06-28 17:46:34 +0100

    Removed unnecessary imports

--- src/dev/nova/nmoyang/Main.java
+++ src/dev/nova/nmoyang/Main.java
@@ -1,13 +1,10 @@
 package dev.nova.nmoyang;
 
-import java.util.ArrayList;
-
 public class Main {
 
     public static void main(String[] args) {
         if(args.length == 0){
             guiMode();
-
         }else if (args.length == 1 && args[0].equalsIgnoreCase("consoleMode")){
             consoleMode();
         }else{
