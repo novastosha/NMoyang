bb2f7c388fc496c467104d5efa18ddbdaf4b1471
novastosha <galliigotu@gmail.com>
2021-06-29 18:22:09 +0100

    Created main class for bukkit plugin

---
+++ src/dev/nova/nmoyang/bukkit/MainBukkit.java
@@ -0,0 +1,13 @@
+package dev.nova.nmoyang.bukkit;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ *
+ * This class was purely created for Bukkit
+ *
+ * As bukkit requires the jar to be a plugin.
+ *
+ */
+public class MainBukkit extends JavaPlugin {
+}
