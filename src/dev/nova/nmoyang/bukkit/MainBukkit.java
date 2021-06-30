e505fa3b8167dd7f45089a720b8a452c66480b24
novastosha <galliigotu@gmail.com>
2021-06-30 16:14:58 +0100

    Added messages on the disable / enable of the plugin.

--- src/dev/nova/nmoyang/bukkit/MainBukkit.java
+++ src/dev/nova/nmoyang/bukkit/MainBukkit.java
@@ -1,5 +1,7 @@
 package dev.nova.nmoyang.bukkit;
 
+import dev.nova.nmoyang.api.Mojang;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 /**
@@ -10,4 +12,14 @@ import org.bukkit.plugin.java.JavaPlugin;
  *
  */
 public class MainBukkit extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        Bukkit.getConsoleSender().sendMessage("§7[§cNMoyangAPI§7] The API's Bukkit plugin has been enabled!");
+    }
+
+    @Override
+    public void onDisable() {
+        Bukkit.getConsoleSender().sendMessage("§7[§cNMoyangAPI§7] The API's Bukkit plugin has been disabled!");
+    }
 }
