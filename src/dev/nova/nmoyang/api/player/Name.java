03db52cce25c94f0826e126caa269dbf5d965024
novastosha <galliigotu@gmail.com>
2021-07-01 18:03:43 +0100

    Modified Java Docs.

--- src/dev/nova/nmoyang/api/player/Name.java
+++ src/dev/nova/nmoyang/api/player/Name.java
@@ -12,7 +12,7 @@ public class Name {
      * Creates a name for name history purposes.
      *
      * @param name The name string.
-     * @param changedAt The time it got changed. (When null it means that is the inital name, Mojang won't record this because it would be the same time the account got bought)
+     * @param changedAt The time it got changed.
      */
     public Name(String name, @Nullable long changedAt){
         this.name = name;
