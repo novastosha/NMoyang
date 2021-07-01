987e1b3fd4e6ec16efdce7268d129430b37fde7d
novastosha <galliigotu@gmail.com>
2021-07-01 19:02:20 +0100

    A command to display stats for a sale type (for console mode)

---
+++ src/dev/nova/nmoyang/console/commands/StatsCommand.java
@@ -0,0 +1,42 @@
+package dev.nova.nmoyang.console.commands;
+
+import dev.nova.nmoyang.Main;
+import dev.nova.nmoyang.api.stats.SaleStatistics;
+import dev.nova.nmoyang.api.stats.SaleStatisticsType;
+import dev.nova.nmoyang.console.Command;
+
+public class StatsCommand extends Command {
+
+    public StatsCommand() {
+        super("stats","Get stats about sales.");
+    }
+
+    @Override
+    public void execute(String[] args) {
+        if(args.length == 0){
+            System.out.println("Please specify a type!");
+            return;
+        }
+
+        try{
+
+            SaleStatisticsType type = SaleStatisticsType.valueOf(args[0].toUpperCase());
+
+            SaleStatistics saleStatistics = Main.getAPI().getSaleStatistics(type);
+
+            if(saleStatistics != null) {
+
+                System.out.println("Statistics for: " + type.name());
+                System.out.println();
+                System.out.println("Total: " +saleStatistics.getTotal());
+                System.out.println("24 hours: " +saleStatistics.getLastDay());
+                System.out.println("Avg. per second: " +saleStatistics.getAveragePerSecond());
+            }else{
+                System.out.println("Couldn't get stats!");
+            }
+
+        }catch (IllegalArgumentException e) {
+            System.out.println("Unknown type!");
+        }
+    }
+}
