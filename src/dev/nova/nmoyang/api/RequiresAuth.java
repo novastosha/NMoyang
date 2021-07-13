a759ac8c40b7881a639172283c4545b134a4d81c
novastosha <galliigotu@gmail.com>
2021-07-13 09:12:24 +0100

    RequiresAuth annonation now can only be added to methods.

--- src/dev/nova/nmoyang/api/RequiresAuth.java
+++ src/dev/nova/nmoyang/api/RequiresAuth.java
@@ -1,9 +1,12 @@
 package dev.nova.nmoyang.api;
 
+import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
 public @interface RequiresAuth {
 
     boolean required() default true;
